0208ef35a10222cf980f9f0510cb940ad1917df8
ccc254 <152591705+ccc254@users.noreply.github.com>
2024-02-18 23:20:47 +0700

    Add files via upload

---
+++ chanon/game.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "spaceship.hpp"
+#include "obstacle.hpp"
+#include "alien.hpp"
+#include "mysteryship.hpp"
+
+class Game {
+    public:
+        Game();
+        ~Game();
+        void Draw();
+        void Update();
+        void HandleInput();
+        bool run;
+        ///
+        int lives; //Add Score
+        int score; //Add Score
+        ///
+        int highscore; //Add Highscore
+        ///
+        Music music; //Add Sounds
+        ///
+    private:
+        void DeleteInactiveLasers();
+        std::vector<Obstacle> CreateObstacles();
+        std::vector<Alien> CreateAliens();
+        void MoveAliens();
+        void MoveDownAliens(int distance); 
+        void AlienShootLaser();
+        void CheckForCollisions();
+        void GameOver();
+        void Reset();
+        void InitGame();
+        ///
+        void checkForHighscore();//Add Highscore
+        ///
+        void saveHighscoreToFile(int highscore); //Add Highscore
+        ///
+        int loadHighscoreFromFile(); //Add Highscore
+        ///
+        Spaceship spaceship;
+        std::vector<Obstacle> obstacles;
+        std::vector<Alien> aliens;
+        int aliensDirection;
+        std::vector<Laser> alienLasers;
+        constexpr static float alienLaserShootInterval = 0.35;
+        float timeLastAlienFired;
+        MysteryShip mysteryship;
+        float mysteryShipSpawnInterval;
+        float timeLastSpawn;
+        //Add Sounds
+        Sound explosionSound;
+        ///
+};
