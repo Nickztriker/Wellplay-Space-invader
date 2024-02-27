8c8ceb384f8999cf28b32d22af19537f1b9a1485
Chanon <natchanon.thamkankang@gmail.com>
2024-02-27 21:56:55 +0700

    change

--- chanon/game.hpp
+++ chanon/game.hpp
@@ -6,41 +6,14 @@
 
 class Game {
     public:
-        Game();
-        ~Game();
-        void Draw();
-        void Update();
-        void HandleInput();
-        bool run;
-        int lives; 
         int score; //[Add Score]ประกาศตัวแปร score
         int highscore; //[Add Highscore]
         Music music; //[Add Sounds]
 
     private:
-        void DeleteInactiveLasers();
-        std::vector<Obstacle> CreateObstacles();
-        std::vector<Alien> CreateAliens();
-        void MoveAliens();
-        void MoveDownAliens(int distance); 
-        void AlienShootLaser();
-        void CheckForCollisions();
-        void GameOver();
-        void Reset();
-        void InitGame();
         void checkForHighscore();//[Add Highscore] 
         void saveHighscoreToFile(int highscore); //[Add Highscore]
         int loadHighscoreFromFile(); //[Add Highscore]
-        Spaceship spaceship;
-        std::vector<Obstacle> obstacles;
-        std::vector<Alien> aliens;
-        int aliensDirection;
-        std::vector<Laser> alienLasers;
-        constexpr static float alienLaserShootInterval = 0.35;
-        float timeLastAlienFired;
-        MysteryShip mysteryship;
-        float mysteryShipSpawnInterval;
-        float timeLastSpawn;
         Sound explosionSound;//[Add Sounds] Explosion Sound
         
 };
--- chanon/main.cpp
+++ chanon/main.cpp
@@ -12,42 +12,11 @@ std::string FormatWithLeadingZeros(int number, int width) {
 
 int main()
 {
-    Color grey = {29, 29, 27, 255};
-    Color yellow = {243, 216, 63, 255};
-    int offset = 50;
-    int windowWidth = 750;
-    int windowHeight = 700;
-
-    InitWindow(windowWidth + offset, windowHeight + 2 * offset, "C++ Space Invaders");
+    
     InitAudioDevice();//[Add Sounds]
-
-    Font font = LoadFontEx("Font/monogram.ttf", 64, 0, 0);
-    Texture2D spaceshipImage = LoadTexture("Graphics/spaceship.png");
-
-    SetTargetFPS(60);
-
-    Game game;
 
     while(WindowShouldClose() == false) {
         UpdateMusicStream(game.music); //[Add Sounds]
-        game.HandleInput();
-        game.Update();
-        BeginDrawing();
-        ClearBackground(grey);
-        DrawRectangleRoundedLines({10, 10, 780, 780}, 0.18f, 20, 2, yellow);
-        DrawLineEx({25, 730}, {775, 730}, 3, yellow);
-
-        if(game.run){
-            DrawTextEx(font, "LEVEL 01", {570, 740}, 34, 2, yellow);
-        } else {
-            DrawTextEx(font, "GAME OVER", {570, 740}, 34, 2, yellow);
-        }
-        float x = 50.0;
-        for(int i = 0; i < game.lives; i ++) {
-            DrawTextureV(spaceshipImage, {x, 745}, WHITE);
-            x += 50;
-        }
-        
         //[Add Score] : Display Score on the screen (ฟอนต์,คำ,{ขนาด,ขนาด})
         DrawTextEx(font, "SCORE", {50, 15}, 34, 2, yellow);
         std::string scoreText = FormatWithLeadingZeros(game.score, 5); //เรียกใช้ function FormatWithLeadingZeros 
@@ -62,6 +31,5 @@ int main()
         EndDrawing();
     }
 
-    CloseWindow();
     CloseAudioDevice();//[Add Sounds]ปิดเสียงเมื่อออกเกม
 }
